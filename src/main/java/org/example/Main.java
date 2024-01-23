730fd5cb15d43b8c3632a07950d9849f1be77248
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-01-23 21:24:12 +0100

    Minuty i Sekundy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,13 @@
 package org.example;
 
+import org.lesson4ex2.MinutesAndSeconds;
 import org.lesson4ex2.Movie;
 
 public class Main {
     public static void main(String[] args) {
-        Movie movie1 = new Movie();
+        MinutesAndSeconds seconds = new MinutesAndSeconds(2);
+        System.out.println(seconds);
 
-        movie1.setTitle("Pretty Woman");
-        movie1.setDirectorName("Jan Kowalski");
-        movie1.setDuration(60);
-        movie1.setYearOfProduction(2000);
-
-        Movie movie2 = new Movie();
-
-        movie2.setTitle("Nothing Hill");
-        movie2.setDirectorName("Alicja Krupa");
-        movie2.setDuration(45);
-        movie2.setYearOfProduction(2023);
-
-        System.out.println(movie1.toString());
-        System.out.println(movie2.toString());
 
     }
 
