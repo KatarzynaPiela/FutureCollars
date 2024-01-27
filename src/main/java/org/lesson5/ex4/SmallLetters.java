04ad6181b3ffba5ac388d1a5e45217cc65f8cab5
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-01-27 19:47:41 +0100

    lesson5ex4

---
+++ src/main/java/org/lesson5/ex4/SmallLetters.java
@@ -0,0 +1,14 @@
+package org.lesson5.ex4;
+
+public class SmallLetters implements TextFormatter {
+    public SmallLetters() {
+
+    }
+
+    @Override
+    public String formatText(String tex) {
+        String text = null;
+        return text.toLowerCase();
+
+    }
+}
---
+++ src/main/java/org/lesson5/ex4/TextFormatter.java
@@ -0,0 +1,5 @@
+package org.lesson5.ex4;
+
+public interface TextFormatter {
+    String formatText(String text);
+}
