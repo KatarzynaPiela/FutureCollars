eaedaea147dd5bef9e5c7907377fc66ff2ade019
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-02-03 19:23:36 +0100

    lesson7ex1

---
+++ src/main/java/org/lesson7/ex1/FileExample.java
@@ -0,0 +1,11 @@
+package org.lesson7.ex1;
+
+import java.io.File;
+
+public class FileExample {
+
+    public static void main(String[]args) {
+        File file = new File("C:\\Users\\48576\\Documents\\Repozytorium\\FutureCollars\\src\\main\\java\\org\\lesson7\\ex1\\data.txt");
+        }
+    }
+
