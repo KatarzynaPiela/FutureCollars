6ed46d3f31f09e107bac44856ccf0a63675f44bf
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-02-03 20:41:10 +0100

    lesson7ex3

---
+++ src/main/java/org/lesson7/ex3/data/FileExample.java
@@ -0,0 +1,14 @@
+package org.lesson7.ex3.data;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+public class FileExample {
+    public static void main(String[] args) throws IOException {
+        String filePath = "C:\\Users\\48576\\Documents\\Repozytorium\\FutureCollars\\src\\main\\java\\org\\lesson7\\ex3\\data\\data.txt";
+        File file = new File(filePath);
+        String text = Files.readString(Path.of(filePath));
+        System.out.println(text);
+    }
+}
---
+++ src/main/java/org/lesson7/ex3/data/I0Expection.java
@@ -0,0 +1,4 @@
+package org.lesson7.ex3.data;
+
+public class I0Expection extends Exception {
+}
