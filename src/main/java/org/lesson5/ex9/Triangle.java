cfe4c97e7019b3870dd689d0f05112369663dfe6
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-02-03 17:05:56 +0100

    lesson5ex9

---
+++ src/main/java/org/lesson5/ex9/Triangle.java
@@ -0,0 +1,15 @@
+package org.lesson5.ex9;
+
+public class Triangle {
+    private double width;
+    private double height;
+
+    public Triangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    @Override
+    public String toString() {
+        return "Triangle{width=<wartośćPolaWitdh>, height=<wartośćPolaHeight>}";
+    }
+}
