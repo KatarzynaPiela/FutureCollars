b39cf341996762fb36c63be5f1c390dcfd358495
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-02-03 16:52:41 +0100

    lesson5ex6

---
+++ src/main/java/org/lesson5/ex6/BillCalculator.java
@@ -0,0 +1,16 @@
+package org.lesson5.ex6;
+
+public class BillCalculator {
+    static double calculate(double billValue, float serviceFree) {
+        return billValue + serviceFree;
+    }
+    static double calculate(double billValue, float serviceFree, double discount) {
+        return billValue + serviceFree + discount;
+    }
+    static double calculate(double billValue, float serviceFree, short takeawayPackaging) {
+        return billValue + serviceFree + takeawayPackaging;
+    }
+}
+
+
+
