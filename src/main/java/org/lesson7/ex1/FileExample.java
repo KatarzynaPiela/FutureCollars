b4ab7604127a3b5b93373c896cdf9cb399f30c70
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-02-10 18:58:35 +0100

    poprawka zadania 7 ex1

--- src/main/java/org/lesson7/ex1/FileExample.java
+++ src/main/java/org/lesson7/ex1/FileExample.java
@@ -6,6 +6,7 @@ public class FileExample {
 
     public static void main(String[]args) {
         File file = new File("C:\\Users\\48576\\Documents\\Repozytorium\\FutureCollars\\src\\main\\java\\org\\lesson7\\ex1\\data.txt");
+        System.out.println(file.canWrite());
         }
     }
 
