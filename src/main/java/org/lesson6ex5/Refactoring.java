3b83051c7a6adb34703b382fb45eecb9bf31f7ec
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-02-10 18:49:55 +0100

    poprawka zadania 6 ex0

---
+++ src/main/java/org/lesson6ex5/Refactoring.java
@@ -0,0 +1,15 @@
+package org.lesson6ex5;{
+
+    public class Bicycle extends
+
+        @Override
+        protected void startEngine() {
+            throw new UnsupportedOperationException("Bicycle does not have engine");
+        }
+
+        @Override
+        protected void stopEngine() {
+            throw new UnsupportedOperationException("Bicycle does not have engine");
+        }
+    }
+
