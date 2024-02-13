ee7fa61cf740cb9a340d45c2500078d06c1a0ecf
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-02-13 21:52:51 +0100

    lesson8ex1

--- src/main/java/org/lesson7/ex3/data/FileExample.java
+++ src/main/java/org/lesson7/ex3/data/FileExample.java
@@ -1,15 +1,20 @@
 package org.lesson7.ex3.data;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Files;
 
 public class FileExample {
-    public static void main(String[] args) throws IOException {
-        String filePath = "C:\\Users\\48576\\Documents\\Repozytorium\\FutureCollars\\src\\main\\java\\org\\lesson7\\ex3\\data\\data.txt";
-        File file = new File(filePath);
-        String text = Files.readString(Path.of(filePath));
-        System.out.println(text);
+    public static void main(String[] args) {
+
+        try {
+            String filePath = "C:\\Users\\48576\\Documents\\Repozytorium\\FutureCollars\\src\\main\\java\\org\\lesson7\\ex3\\data\\data.txt";
+            BufferedReader
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
