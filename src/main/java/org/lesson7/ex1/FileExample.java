27f1af426fa95ad76709835825b5eda95d262386
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-02-21 10:37:58 +0100

    lesson7ex1

--- src/main/java/org/lesson7/ex1/FileExample.java
+++ src/main/java/org/lesson7/ex1/FileExample.java
@@ -1,12 +1,22 @@
 package org.lesson7.ex1;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class FileExample {
+    public static void main(String[] args) throws IOException {
+        String path = "C:\\Users\\48576\\Documents\\Repozytorium\\FutureCollars\\src\\main\\java\\org\\lesson7\\ex1\\data.txt";
+        String text = "Kurs\n" +
+                "Java\n" +
+                "Lekcja 6\n" +
+                "Pliki\n" +
+                "Wyjątki\n" +
+                "Pliki\n" +
+                "Koniec pliku";
 
-    public static void main(String[]args) {
-        File file = new File("C:\\Users\\48576\\Documents\\Repozytorium\\FutureCollars\\src\\main\\java\\org\\lesson7\\ex1\\data.txt");
-        System.out.println(file.canWrite());
-        }
+        Files.writeString(Path.of(path), text);
     }
+}
 
