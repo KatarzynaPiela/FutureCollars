6e48a3b9f5f97beafdc6b9e14c6e75f3c228adbf
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-03-02 21:36:29 +0100

    poprawka

--- src/main/java/org/lesson7/ex3/data/FileExample.java
+++ src/main/java/org/lesson7/ex3/data/FileExample.java
@@ -1,25 +1,42 @@
 package org.lesson7.ex3.data;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileExample {
     private static BufferedReader BufferedReader;
+    static String pathName = "data.txt";
 
     public static void main(String[] args) {
+        File myFile = new File(pathName);
+        List<String> lineText = new ArrayList<>();
+        String text;
+        if (pathName == null || pathName.isBlank()) {
+            throw new NullPointerException("Wrong path, null or empty!");
+        } else if (!myFile.exists()) {
+            throw new IllegalArgumentException("There's no such file exist");
+        } else {
+            try (BufferedReader reader = new BufferedReader(new FileReader(pathName))) {
+                while ((text = reader.readLine()) != null) { // ok to zapier
+                    lineText.add(text);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println(e.getMessage());
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
 
-        try {
-            String filePath = "C:\\Users\\48576\\Documents\\Repozytorium\\FutureCollars\\src\\main\\java\\org\\lesson7\\ex3\\data\\data.txt";
-            int lineNumber = 0;
-            String line;
-            BufferedReader = new BufferedReader(new FileReader(filePath));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            for (int i = 0; i < lineText.size(); i += 2) {
+                System.out.println(lineText.get(i));
+            }
+
+
         }
+
 
     }
 }
