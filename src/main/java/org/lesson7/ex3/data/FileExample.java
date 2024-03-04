bfdba428a70c0b105af80c66d219d077faa4dd4d
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-03-04 17:20:20 +0100

    poprawka

--- src/main/java/org/lesson7/ex3/data/FileExample.java
+++ src/main/java/org/lesson7/ex3/data/FileExample.java
@@ -8,35 +8,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileExample {
-    private static BufferedReader BufferedReader;
     static String pathName = "data.txt";
 
     public static void main(String[] args) {
-        File myFile = new File(pathName);
+
         List<String> lineText = new ArrayList<>();
+
         String text;
-        if (pathName == null || pathName.isBlank()) {
-            throw new NullPointerException("Wrong path, null or empty!");
-        } else if (!myFile.exists()) {
-            throw new IllegalArgumentException("There's no such file exist");
-        } else {
-            try (BufferedReader reader = new BufferedReader(new FileReader(pathName))) {
-                while ((text = reader.readLine()) != null) {
-                    lineText.add(text);
-                }
-            } catch (FileNotFoundException e) {
-                System.out.println(e.getMessage());
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
+        try (BufferedReader reader = new BufferedReader(new FileReader(pathName))) {
+            while ((text = reader.readLine()) != null) {
+                lineText.add(text);
             }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
-            for (int i = 0; i < lineText.size(); i += 2) {
-                System.out.println(lineText.get(i));
-            }
-
-
+        for (int i = 0; i < lineText.size(); i += 2) {
+            System.out.println(lineText.get(i));
         }
 
 
     }
+
+
 }
+
